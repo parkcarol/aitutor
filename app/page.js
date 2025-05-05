0978174e06992cfdf5c0f783b920dcbdbffe4a91
Caroline Park <parkcarol@outlook.com>
2025-05-05 14:05:04 -0400

    add logo

--- app/page.js
+++ app/page.js
@@ -658,14 +658,13 @@ export default function Home() {
 
       {/* Navbar */}
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid text-center">
-          <a className="navbar-brand" href="#">aiTutor</a>
+        <div className="container-fluid text-center" style={{ justifyContent: 'center' }}>
+          <Image src={"/logo.png"} alt="Logo" width={100} height={40} className="" />
+          <a className="navbar-brand" href="#"></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-
-
-        </div >
+        </div>
       </nav >
 
       {/* Main Content */}
